5a42d54f54cb707242ecf0f03d17de3323f7827d
kartheek reddy <pagadala.karthikreddy123@gmail.com>
2026-05-27 13:02:43 +0530

    app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ age = int(input("Enter your age: "))
 print("Hello", dad")
 print("You are", age
 if age >= 2:
-    print("You are eligible")
+    print("some")
 else:
     print("You are not eligible")
